5168ee493d2427414d9b96588d3f7a2cb9cbbc01
Vikas Ranjan <vikas434@gmail.com>
2025-12-07 19:24:29 +0530

    Add picsum.photos to Next.js image configuration
    - Added picsum.photos to remotePatterns in next.config.js
    - Fixes error when loading dummy evidence images
    - Allows Next.js Image component to load placeholder images

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'assets.wfcdn.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
     ],
   },
 }
